52116cee4b2ba09bf1b7f9d68a4fd4eb100da453
Alejandro Mery <amery@geeks.cl>
2009-06-01 01:53:27 +0200

    http: added send_301(location) method

--- restingmoon/http.lua
+++ restingmoon/http.lua
@@ -1,7 +1,18 @@
+local r = require "restingmoon.response"
+
 require "os"
 
 module(..., package.seeall)
 
 function timestamp(t)
 		return os.date("!%A %d-%b-%y %T %Z", t)
+end
+
+function send_301(location)
+	local headers = { ["Location"] = location }
+	local res = r.new(301, headers)
+
+	res:write("redirect")
+
+	return res:finish()
 end
