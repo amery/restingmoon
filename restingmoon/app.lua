63452e418ac32603acb124ec978e4608306c3228
Alejandro Mery <amery@geeks.cl>
2009-05-31 15:39:15 +0200

    app: adapted to the last changes in restingmoon

--- restingmoon/app.lua
+++ restingmoon/app.lua
@@ -1,16 +1,22 @@
 require "restingmoon.debug"
+require "restingmoon.log"
+
+require "restingmoon.request"
+
 require "restingmoon.static"
+
+require "lfs"
 
 module(..., package.seeall)
 
-function hello_world(wsapi_env)
+function hello_world(req)
 	local headers = { ["Content-type"] = "text/html" }
 
 	local function hello_text()
 		coroutine.yield("<html><body>")
 		coroutine.yield("<h1>Hello Wsapi!</h1>")
 		coroutine.yield("<pre>")
-		coroutine.yield(table.show(wsapi_env, "wsapi_env"))
+		coroutine.yield(table.show(req, "req"))
 		coroutine.yield("</pre>")
 		coroutine.yield("</body></html>")
 	end
@@ -19,14 +25,42 @@ function hello_world(wsapi_env)
 end
 
 local function run(app, wsapi_env)
-	if (app.overlay) then
-		local status, header, body = restingmoon.static.wsapi_handler(app, wsapi_env)
+	local req = restingmoon.request.new(wsapi_env)
+	local status, header, body
 
-		if (status) then
-			return status, header, body
+	restingmoon.log_response(req)
+
+	--local res = restingmoon.static.wsapi_handler(req)
+
+	if req.document_root ~= "" then
+		local filename, attr
+
+		-- first trying straight matches
+		--
+		filename = req.document_root .. req.path_info
+		attr = lfs.attributes(filename)
+
+		if attr == nil then
+			-- NOP
+		elseif attr.mode == "file" then
+			-- sweet, let's serve it immediately
+			status, header, body = restingmoon.static.wsapi_dispatch_file(filename, attr)
+		--[[
+		elseif attr.mode == "directory" then
+			print("DIR:", filename)
+			]]--
+		else
+			print(table.show(attr, filename))
 		end
 	end
-	return app.app_run(wsapi_env)
+
+	if status == nil then
+		status, header, body = app.app_run(req)
+	end
+
+	restingmoon.log_response(req, status, header["Content-Length"])
+
+	return status, header, body
 end
 
 function new(app)
@@ -40,8 +74,4 @@ function new(app)
 	-- callback to the real app
 	--
 	app.app_run = hello_world
-
-	-- directory for overwriting what is served by the app
-	--
-	app.overlay = "public"
 end
