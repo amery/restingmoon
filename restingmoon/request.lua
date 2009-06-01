eb60d9119197e87d65e5b1f4d3386bbd1150265a
Alejandro Mery <amery@geeks.cl>
2009-06-02 00:15:30 +0200

    request: changed req.path to be a list of tokens

--- restingmoon/request.lua
+++ restingmoon/request.lua
@@ -3,6 +3,7 @@ module(..., package.seeall)
 require "wsapi.request"
 
 function new(app, wsapi_env)
+	local p, t
 
 	-- references to wsapi stuff
 	--
@@ -11,11 +12,16 @@ function new(app, wsapi_env)
 		wsapi_req = wsapi.request.new(wsapi_env),
 	}
 
-	-- path and optional extension, extension wins againts HTTP_ACCEPT
+	-- path list and optional extension, extension wins againts HTTP_ACCEPT
 	--
 	req.path_info = wsapi_env["PATH_INFO"]
 	req.path_ext = req.path_info:match("[^./]%.([^./]+)$")
 	req.path = req.path_ext and req.path_info:sub(1,-1*(#req.path_ext+2)) or req.path_info
+	p = {}
+	for t in req.path:gmatch("([^/]+)") do
+		p[#p+1] = t
+	end
+	req.path = p
 
 	req.method = wsapi_env.REQUEST_METHOD
 
