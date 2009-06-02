15db05b16a938615df6e15a9d1ad20d85b940d77
Alejandro Mery <amery@geeks.cl>
2009-06-02 15:40:39 +0200

    http: implemented some other send_XXX() methods

--- restingmoon/http.lua
+++ restingmoon/http.lua
@@ -8,11 +8,29 @@ function timestamp(t)
 		return os.date("!%A %d-%b-%y %T %Z", t)
 end
 
+function send_200_head(headers)
+	return 200, headers, nil
+end
+
+function send_204(headers)
+	headers["Content-Length"] = 0
+	return 204, headers, nil
+end
+
 function send_301(location)
-	local headers = { ["Location"] = location }
-	local res = r.new(301, headers)
+	local headers = {
+		["Location"] = location,
+		["Content-Length"] = 0,
+	}
 
-	res:write("redirect")
+	return 301, headers, nil
+end
 
-	return res:finish()
+function send_304(headers)
+	return 304, headers, nil
+end
+
+function send_405()
+	headers["Content-Length"] = 0
+	return 405, {}, nil
 end
