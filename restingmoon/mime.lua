01b075e5b9925ac4769a466b933376d19f9dec19
Alejandro Mery <amery@geeks.cl>
2009-06-03 18:42:46 +0200

    mime: introduced parse_media_ranges(), parse_media_range() and parse_mime_type() - with caching

--- restingmoon/mime.lua
+++ restingmoon/mime.lua
@@ -48,3 +48,74 @@ end
 function mime_by_filename(filename)
 	return mime_by_ext(filename:match("[^./]%.([^./]+)$"))
 end
+
+local cache_media_range = {}
+local cache_media_type = {
+	["*/*"] = { type="*", subtype="*" },
+}
+cache_media_type["*"] = cache_media_type["*/*"]
+
+function parse_mime_type(s)
+	if type(s) ~= "string" then
+		return nil
+	elseif not cache_media_type[s] then
+		local t = {}
+		t.type, t.subtype = string.match(s, "^ *([^/]+)/([^/]+) *$")
+
+		if not t.type then
+			io.stderr:write(s, "Invalid mime-type\n")
+			return nil
+		end
+		cache_media_type[s] = t
+	end
+	return cache_media_type[s]
+end
+
+function parse_media_range(s)
+	if not cache_media_range[s] then
+		local t = {}
+
+		for x in string.gmatch(s, " *([^;]+) *") do
+			if not t.type then
+				local ct = parse_mime_type(x)
+				if not ct then
+					return nil
+				end
+
+				t.type, t.subtype, t.params = ct.type, ct.subtype, {q=1}
+			else
+				local k, v = string.match(x," *([^=]+)=([^ ]*)")
+				local n = tonumber(v)
+				if n then
+					t.params[k] = n
+				else
+					t.params[k] = v
+				end
+			end
+		end
+
+		if next(t) ~= nil then
+			if type(t.params.q) ~= "number" or t.params.q <0 or t.params.q > 1 then
+				t.params.q = 1
+			end
+
+			cache_media_range[s]=t
+		else
+			return nil
+		end
+	end
+
+	return cache_media_range[s]
+end
+
+function parse_media_ranges(s)
+	if not cache_media_range[s] then
+		local t = {}
+		for v in string.gmatch(s, " *([^,]+) *") do
+			t[#t+1] = parse_media_range(v)
+		end
+		cache_media_range[s]=t
+	end
+
+	return cache_media_range[s]
+end
