e541b994ab755fe5a578d6295e25f06aaba35a10
Alejandro Mery <amery@geeks.cl>
2009-06-10 02:55:39 +0200

    help: added table.deepcopy() from http://lua-users.org/wiki/CopyTable

--- restingmoon/help.lua
+++ restingmoon/help.lua
@@ -102,3 +102,24 @@ function table.show(t, name, indent)
 	addtocart(t, name, indent)
 	return cart .. autoref
 end
+
+-- Taken from http://lua-users.org/wiki/CopyTable
+--
+function table.deepcopy(object)
+	local lookup_table = {}
+	local function _copy(object)
+		if type(object) ~= "table" then
+			return object
+		elseif lookup_table[object] then
+			return lookup_table[object]
+		end
+
+		local new_table = {}
+		lookup_table[object] = new_table
+		for index, value in pairs(object) do
+			new_table[_copy(index)] = _copy(value)
+		end
+		return setmetatable(new_table, getmetatable(object))
+	end
+	return _copy(object)
+end
