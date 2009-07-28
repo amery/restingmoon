99df29257bda39636b0c643297b0d6331f8d344a
Alejandro Mery <amery@geeks.cl>
2009-07-28 17:30:10 -0400

    model.enum: Added optional filter to html_option()

--- restingmoon/model/enum.lua
+++ restingmoon/model/enum.lua
@@ -10,30 +10,36 @@ function validate(f, v)
 	end
 end
 
-function raw_html_option(enum, current)
+function raw_html_option(enum, current, filter)
 	local function nop() end
 	local function yes()
 		coroutine.yield({})
 	end
 
+	if filter == nil then
+		filter = function() return true end
+	end
+
 	return function()
 		for k, v in pairs(enum) do
-			coroutine.yield{
-				id=k,
-				name=v,
-				current= (k == current) and yes or nop,
-			}
+			if filter(k) then
+				coroutine.yield{
+					id=k,
+					name=v,
+					current= (k == current) and yes or nop,
+				}
+			end
 		end
 	end
 end
 
-function html_option(f, current)
+function html_option(f, current, filter)
 	if type(current) == "table" then
 		-- it's the object
 		current = current[f.name]
 	end
 
-	return raw_html_option(f.enum, current)
+	return raw_html_option(f.enum, current, filter)
 end
 
 function new(mt, name, enum, default)
